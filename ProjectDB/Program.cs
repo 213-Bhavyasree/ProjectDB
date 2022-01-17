ab1c034a26ee5d1f31c5f8f4895547c73f0597aa
lokes <lokes@LAPTOP-J2B9HTQV>
2022-01-17 11:53:01 +0530

    Add project files.

---
+++ ProjectDB/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new CustomersContext())
+            {
+                List<CustomerDetails> customerDetails = context.CustomerDetails.ToList();
+                foreach (var customerDetailss in customerDetails)
+                {
+                    Console.WriteLine(customerDetailss.Address);
+                }
+
+            }
+        }
+    }
+}
